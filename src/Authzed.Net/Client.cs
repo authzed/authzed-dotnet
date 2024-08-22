aa3cb179185b1a5525df5362683b8c061091bf2d
Tanner Stirrat <tstirrat@gmail.com>
2024-08-22 14:42:57 -0600

    Add start of new package

---
+++ src/Authzed.Net/Client.cs
@@ -0,0 +1,12 @@
+﻿namespace Authzed.Net;
+
+
+/// <summary>
+/// Represents a SpiceDB Client for interacting with a SpiceDB instance.
+/// </summary
+public class Client
+{
+    public Client() {
+
+    }
+}
